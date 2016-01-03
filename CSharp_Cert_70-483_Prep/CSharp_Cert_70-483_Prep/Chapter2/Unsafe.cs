e66d8e5c9443cacf4afb6d4fbb6ae5ab01890687
Nick Van Haver <nvhaver@gmail.com>
2016-01-03 11:34:04 +0100

    Added chapter folders. Started on Chapter 2 content.

---
+++ CSharp_Cert_70-483_Prep/CSharp_Cert_70-483_Prep/Chapter2/Unsafe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Cert_70_483_Prep.Chapter2
+{
+    /// <summary>
+    /// This class focusses on the unsafe keyword.
+    /// More information on this keyword can be found on:
+    /// https://msdn.microsoft.com/en-us/library/chfa2zb8.aspx
+    /// </summary>
+    public class Unsafe
+    {
+
+    }
+}
