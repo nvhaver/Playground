868977ce4e88ec4ea8196c9cac1b3e49f00d11d6
Nick Van Haver <nvhaver@gmail.com>
2016-01-04 21:26:05 +0100

    Added parameters example.

---
+++ C#/CSharp_Cert_70-483_Prep/CSharp_Cert_70-483_Prep/Chapter2/Parameters.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Cert_70_483_Prep.Chapter2
+{
+    public class Parameters
+    {
+        public void OptionalParams(int firstArg, string secondArg = "default")
+        {
+            // The second parameter is optional and has a default value.
+        }
+
+        public void CallMethod()
+        {
+            // Switch the order of the parameters using named parameters.
+            OptionalParams(secondArg: "string", firstArg: 2);
+            // Omit the optional parameter. The default will be used.
+            OptionalParams(1);
+        }
+
+    }
+}
