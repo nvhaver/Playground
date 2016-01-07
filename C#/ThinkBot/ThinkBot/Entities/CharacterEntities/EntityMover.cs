f3f732504ed630d7d36edcab80196c5e24950e87
Jelle <kjoorself.goofu@gmail.com>
2016-01-07 22:06:19 +0100

    Removed what's in EntityMover, added to do list
    A lot of work has to be done to get things moving.

--- C#/ThinkBot/ThinkBot/Entities/CharacterEntities/EntityMover.cs
+++ C#/ThinkBot/ThinkBot/Entities/CharacterEntities/EntityMover.cs
@@ -11,72 +11,17 @@ using ThinkBot.Logging;
 namespace ThinkBot.Entities.CharacterEntities
 {
     public static class DudeMover
-    { // should be performed in another thread, so other threads can then utilise the entity's props to update the gui or do calculations.
-
-
-
-        public static void MoveTo(Location targetLoc, Dude dude)
+    {
+		public static void MoveTo(Location targetLoc, Dude dude)
         {
-            int movementspeed = dude.MovementSpeed;
-            double[] modifiers = CalcMovementModifiers(targetLoc, dude.Location);
-            double xModifier = modifiers[0];
-            double yModifier = modifiers[1];
-            PerformMovement(targetLoc, dude, xModifier, yModifier, movementspeed);
+				/* this class was used to move in a dumb way, and it didn't work.
+				   This class should be made so it can be implemented by other classes ;)
+				   This should give them the ability to move through landscapes with realistic pathfinding and line of sight walking
+				
+				   Rethink the entire system how entities are placed, generate impenetrable areas
+				   Implement Theta* for the pathfinding. 
+				*/
         }
-        
-
-        public static void PerformMovement(Location targetLoc, Dude dude, double xMod, double yMod, int moveSpeed)
-        {
-
-            // REWRITE THIS SO A DUDE CAN MOVE USING SMART PATHFINDING
-
-            Location currentLoc = dude.Location;
-
-            while (true)
-            {
-                if ((currentLoc.CordX + xMod > targetLoc.CordX) || (currentLoc.CordX + xMod < targetLoc.CordX))
-                { // if targetX is further than 1 away from the current coord
-                    currentLoc.CordX += xMod;
-                }
-                else
-                { // dude has arrived, target loc becomes dude loc (this is when he's close than 1 count)
-                    currentLoc.CordX = targetLoc.CordX;
-                }
-
-                if ((currentLoc.CordY + yMod > targetLoc.CordY) || (currentLoc.CordY + yMod < targetLoc.CordY))
-                { // if targetX is further than 1 away from the current coord
-                    currentLoc.CordY += yMod;
-                }
-                else
-                { // dude has arrived, target loc becomes dude loc (this is when he's close than 1 count)
-                    currentLoc.CordY = targetLoc.CordY;
-                }
-                if ((currentLoc.CordX == targetLoc.CordX) && (currentLoc.CordY == targetLoc.CordY))
-                {
-                    break;
-                }
-
-                Logger.WriteActivityLog(3, "MOVED " + currentLoc.ToString());
-                Thread.Sleep(901 - moveSpeed);
-                
-            }
-
-
-        }
-
-
-
-        public static double[] CalcMovementModifiers(Location targetLoc, Location currentLoc)
-        {
-            double xModifier =0, yModifier =0;
-            // This code was shit. It should've given 1 modifier that either outputs 1 or -1, 
-            // the other one 0.xx... or -0.xx... relative to it.
-            // Basically this and perform movement only make it possible to move in straight lines.
-            // Has to be rewritten to be a true pathfinder
-
-
-            // return both the modifiers.
-            return new double[]{ xModifier, yModifier};
-        }
+      
     }
 }
